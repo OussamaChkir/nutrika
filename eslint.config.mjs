dcdc3168efebc46b756e96767c224f7acf82be37
oussama <oussama501chkir@gmail.com>
2026-02-08 12:20:41 +0100

    fix deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
 import storybook from "eslint-plugin-storybook";
 
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
